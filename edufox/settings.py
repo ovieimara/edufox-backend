677b5615613a776bf2f9367aefb4cf5e8d4e1fb8
ovie <oimara@enprohsoft.com>
2023-02-05 18:35:13 +0100

    db name update

--- edufox/settings.py
+++ edufox/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'edufox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME_TEST'),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST_TEST'),
