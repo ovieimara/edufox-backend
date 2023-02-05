78c1b5b17fb1ff6024713089d77af07185330586
ovie <oimara@enprohsoft.com>
2023-02-05 22:13:18 +0100

    allowed host

--- edufox/settings.py
+++ edufox/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api-service-5wasy3cpxq-uc.a.run.app']
 
 
 # Application definition
