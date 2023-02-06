f8a8b9add188af4388349b54c7a6e9daae615307
ovie <oimara@enprohsoft.com>
2023-02-06 12:53:15 +0100

    db

--- edufox/settings.py
+++ edufox/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api-service-5wasy3cpxq-uc.a.run.app', '127.0.0.1']
+ALLOWED_HOSTS = ['https://api-service-5wasy3cpxq-uc.a.run.app', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     'https://api-service-5wasy3cpxq-uc.a.run.app'
 ]
