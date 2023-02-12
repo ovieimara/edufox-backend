d5e41eb51263f724a729069babf78012d0a1ce4f
ovie <oimara@enprohsoft.com>
2023-02-12 15:52:56 +0100

    env USE_CLOUD_SQL_AUTH_PROXY

--- edufox/settings.py
+++ edufox/settings.py
@@ -71,6 +71,7 @@ except google.auth.exceptions.DefaultCredentialsError:
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = env('DEBUG')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # SECURITY WARNING: It's recommended that you use this when
