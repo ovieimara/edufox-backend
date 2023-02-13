8e8913b72556cebd5ae825cef4790b8f0417371a
ovie <oimara@enprohsoft.com>
2023-02-13 09:17:28 +0100

    DATABASE_URL

--- edufox/settings.py
+++ edufox/settings.py
@@ -35,6 +35,7 @@ env_file = os.path.join(BASE_DIR, ".env")
 # Attempt to load the Project ID into the environment, safely failing on error.
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./creds.json"
 os.environ['USE_CLOUD_SQL_AUTH_PROXY'] = 'true'
+os.environ['DATABASE_URL'] = ''
 
 try:
     _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
