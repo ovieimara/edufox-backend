5f5a357544574141eed18be9a4a003c74fd2f464
ovie <oimara@enprohsoft.com>
2023-02-13 13:11:47 +0100

    USE_CLOUD_SQL_AUTH_PROXY

--- edufox/settings.py
+++ edufox/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 placeholder = (
         f"SECRET_KEY=django-insecure-5xh$hw9%n$huk$mql=%r7p@dxefh9+hleb7yb$eo_6p)r*$dn^\n"
         "GS_BUCKET_NAME=edufox-bucket\n"
-        f"DATABASE_URL=postgres://admin:_edufox@123A@//cloudsql/edufox-services:us-central1:edufox-db-instance/edufox_db"
+        f"DATABASE_URL=ab"
     )
 env.read_env(io.StringIO(placeholder))
 
