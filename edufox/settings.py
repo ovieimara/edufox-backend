fcee65f1e493a6843a55118190022f3520ad2231
ovie <oimara@enprohsoft.com>
2023-02-13 17:49:57 +0100

    ci

--- edufox/settings.py
+++ edufox/settings.py
@@ -106,7 +106,7 @@ env.read_env(io.StringIO(payload))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = True
-print('ENV_STATUS:', env)
+print('ENV_STATUS:', env.ENVIRON)
 
 SECRET_KEY = env("SECRET_KEY")
 GOOGLE_APPLICATION_CREDENTIALS = env('GOOGLE_APPLICATION_CREDENTIALS')
