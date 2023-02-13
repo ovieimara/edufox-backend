8fdaeb4b39c62a6626e684f217f172742669cb57
ovie <oimara@enprohsoft.com>
2023-02-13 20:16:32 +0100

    ci

--- edufox/settings.py
+++ edufox/settings.py
@@ -91,9 +91,9 @@ os.environ['USE_CLOUD_SQL_AUTH_PROXY'] = 'true'
 # elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     # Pull secrets from Secret Manager
 
-project_id = os.env("GOOGLE_CLOUD_PROJECT")
+# project_id = os.env("GOOGLE_CLOUD_PROJECT")
 # print('project_id', project_id)
-# project_id = "edufox-services"
+project_id = "edufox-services"
 client = secretmanager.SecretManagerServiceClient()
 
 # service_account_name = f"projects/{project_id}/secrets/SERVICE_ACCOUNT/versions/latest"
