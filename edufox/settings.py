f73928687eba709bac409d1aba66e552194a3bea
ovie <oimara@enprohsoft.com>
2023-02-13 10:04:37 +0100

    USE_CLOUD_SQL_AUTH_PROXY

--- edufox/settings.py
+++ edufox/settings.py
@@ -35,7 +35,7 @@ env_file = os.path.join(BASE_DIR, ".env")
 # Attempt to load the Project ID into the environment, safely failing on error.
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./creds.json"
 os.environ['USE_CLOUD_SQL_AUTH_PROXY'] = 'true'
-# os.environ['DATABASE_URL'] = ''
+os.environ['DATABASE_URL'] = ''
 # os.environ['GS_BUCKET_NAME'] = ''
 # os.environ['EMAIL_BACKEND'] = ''
 # os.environ['EMAIL_HOST'] = ''
