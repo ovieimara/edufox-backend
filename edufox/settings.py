9f98c23e45d2d524c7726d02ce91afd294f3d2fa
ovie <oimara@enprohsoft.com>
2023-09-07 09:46:15 +0100

    settings

--- edufox/settings.py
+++ edufox/settings.py
@@ -61,14 +61,16 @@ PROJECT_ID = "edufox-services"
 # env.read_env(io.StringIO(payload))
 
 # use google cloud secrets for environment variable
-# googleCloudSecretRepo = GoogleCloudSecretRepo("django_settings", PROJECT_ID)
-# env, read_env, secrets_uri = googleCloudSecretRepo.getEnvironmentVariables()
-# read_env(secrets_uri)
-
-# use local env for environment variable
-localCredentialsRepo = LocalCredentialsRepo()
-env, read_env = localCredentialsRepo.getEnvironmentVariables()
-read_env()
+if os.environ['USE_CLOUD_SQL_AUTH_PROXY']:
+    googleCloudSecretRepo = GoogleCloudSecretRepo(
+        "django_settings", PROJECT_ID)
+    env, read_env, secrets_uri = googleCloudSecretRepo.getEnvironmentVariables()
+    read_env(secrets_uri)
+else:
+    # use local env for environment variable
+    localCredentialsRepo = LocalCredentialsRepo()
+    env, read_env = localCredentialsRepo.getEnvironmentVariables()
+    read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = True
